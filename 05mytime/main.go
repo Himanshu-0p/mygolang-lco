c368b82589a87b25d5c0666ccdc930087ff39cf2
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-02-19 23:39:40 +0530

    ..

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -9,6 +9,10 @@ func main() {
 	fmt.Println("Current time is")
 
 	presentTime := time.Now()
-
+    fmt.Println(presentTime)
 	fmt.Println(presentTime.Format("01-02-2006 Monday 15:05:02"))
+
+	createdDate := time.Date(2020,time.August,10,23,23,0,0,time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
 }
